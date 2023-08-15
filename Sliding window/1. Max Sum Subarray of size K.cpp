2eac118a8115688749abf82602db54f928ef7a47
Sahil Lather <sahillather002@gmail.com>
2023-08-15 15:51:58 +0530

    Create 1. Max Sum Subarray of size K.cpp

---
+++ Sliding window/1. Max Sum Subarray of size K.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int k, vector<int> &arr , int n){
+        // code here 
+        int i=0,j=0;
+        long sum=0;
+        long maxi=INT_MIN;
+        while(j<n){
+            sum+=arr[j];
+            if(j-i+1<k)j++;
+            else if(j-i+1==k){
+                maxi=max(maxi,sum);
+                sum-=arr[i];
+                j++;
+                i++;
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
