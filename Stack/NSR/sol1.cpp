e72335420475b24c3e91e622d9ed8402f85851e6
Sahil Lather <sahillather002@gmail.com>
2023-08-20 17:27:06 +0530

    Create sol1.cpp

---
+++ Stack/NSR/sol1.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        // Your code goes here
+        vector<int>v(n,-1);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()){
+                if(st.top()<arr[i]){
+                    v[i]=st.top();
+                    break;
+                }
+                else st.pop();
+            }
+            st.push(arr[i]);
+        }
+        return v;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+
+// } Driver Code Ends
