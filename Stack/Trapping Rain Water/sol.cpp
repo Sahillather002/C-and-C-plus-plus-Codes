907f3bba8cb191bf54975e1d5017304a35f91630
Sahil Lather <sahillather002@gmail.com>
2023-08-20 23:02:09 +0530

    Create sol.cpp

---
+++ Stack/Trapping Rain Water/sol.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        // code here
+        vector<int>maxl(n),maxr(n);
+        maxl[0]=arr[0];
+        maxr[n-1]=arr[n-1];
+        for(int i=1;i<n;i++){
+            maxl[i]=max(maxl[i-1],arr[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            maxr[i]=max(maxr[i+1],arr[i]);
+        }
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            ans+=min(maxl[i],maxr[i])-arr[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
