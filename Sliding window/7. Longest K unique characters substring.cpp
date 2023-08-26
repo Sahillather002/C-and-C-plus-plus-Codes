129be95adc01b1603d1d0b6772731c390a717053
Sahil Lather <sahillather002@gmail.com>
2023-08-26 09:34:06 +0530

    Update 7. Longest K unique characters substring.cpp

--- Sliding window/7. Longest K unique characters substring.cpp
+++ Sliding window/7. Longest K unique characters substring.cpp
@@ -50,3 +50,34 @@ int main() {
 }
 
 // } Driver Code Ends
+
+
+# Little shorter code
+
+```
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+    // your code here
+    unordered_map<char,int>mp;
+    int ans=-1;
+    int i=0,j=0,n=s.size();
+    while(j<n){
+        mp[s[j]]++;
+        if(mp.size()==k){
+            ans=max(ans,j-i+1);
+        }
+        else if(mp.size()>k){
+            while(mp.size()>k){
+                mp[s[i]]--;
+                if(mp[s[i]]==0)mp.erase(s[i]);
+                i++;
+            }
+        }
+        j++;
+    }
+    return ans;
+    }
+};
+
+```
